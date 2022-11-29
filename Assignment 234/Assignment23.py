b6e2960a291df2b0b2dd1fb314cd22bb462f0ccc
Renita Kurian <bluecoder1206@gmail.com>
2022-11-30 00:40:13 +0530

    Added chinese postman assignment

--- Assignment 234/Assignment23.py
+++ Assignment 234/Assignment23.py
@@ -128,12 +128,10 @@ def Chinese_Postman(graph):
 
 
 # Graph as Adjacency Matrix
-graph =                [[0, 3, 1, 0, 2, 0], 
-                        [3, 0, 0, 1, 0, 6], 
-                        [1, 0, 0, 0, 2, 0], 
-                        [0, 1, 0, 0, 0, 1], 
-                        [2, 0, 2, 0, 0, 4], 
-                        [0, 6, 0, 1, 4, 0]]; 
+graph =                [[0, 3, 1, 0], 
+                        [3, 0, 3, 1], 
+                        [1, 3, 0, 0], 
+                        [0, 1, 0, 0]]; 
 
 
 print("Shortest Distance to Covered: ",Chinese_Postman(graph))
